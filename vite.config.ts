f531cd0c82c04cb74c3a8a96e99a85a0ee40bfbb
Prudhvini-K <sriprudhvini@gmail.com>
2025-07-14 20:57:10 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/screenshot': 'http://localhost:3001',
+      '/health': 'http://localhost:3001'
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
